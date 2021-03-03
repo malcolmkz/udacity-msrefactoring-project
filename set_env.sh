3d2fa6720cb6f40e83f616cbe6d6fb6a43dd0261
Malek KAZDZAGHLI <malek.kazdaghli@sofrecom.com>
2021-03-03 23:38:37 +0100

    committing screenshots

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres-password
+export POSTGRES_HOST=project-db.cte2vtyseww7.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=project-db
+export AWS_BUCKET=arn:aws:s3:::project-mkdev-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=secret
+export URL=http://localhost:8100
